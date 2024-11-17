333b9c38dd455909cfb396ba5fbeaaa6d616ae26
shiro-kreishi <vadas25@yandex.ru>
2024-11-17 17:53:16 +0500

    add paginator for approve_users

--- src/apps/users/views/approve_users.py
+++ src/apps/users/views/approve_users.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import ListView
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 User = get_user_model()
 
@@ -12,6 +13,7 @@ class ApproveUsersListView(UserPassesTestMixin, ListView):
     template_name = "users/approve_users.html"
     context_object_name = "users"
     queryset = User.objects.filter(is_active=False)
+    paginate_by = 10  # Пагинация по 10 пользователей
 
     def test_func(self):
         return self.request.user.is_staff
@@ -19,6 +21,10 @@ class ApproveUsersListView(UserPassesTestMixin, ListView):
     def handle_no_permission(self):
         messages.error(self.request, "У вас нет прав для доступа к этой странице.")
         return redirect('homepage')
+
+    def get_queryset(self):
+        # Вы можете обновить queryset с учетом фильтров, если нужно.
+        return User.objects.filter(is_active=False)
 
     def post(self, request, *args, **kwargs):
         user_ids = request.POST.getlist('user_ids')
@@ -31,3 +37,14 @@ class ApproveUsersListView(UserPassesTestMixin, ListView):
             messages.warning(request, "Не выбрано ни одного пользователя.")
 
         return redirect('approve_users_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = Paginator(self.get_queryset(), self.paginate_by)  # Создание пагинатора
+        page_number = self.request.GET.get('page')  # Получаем номер страницы из GET-запроса
+        page_obj = paginator.get_page(page_number)  # Получаем страницу с пользователями
+
+        context['users'] = page_obj.object_list  # Перезаписываем пользователей с учетом пагинации
+        context['page_obj'] = page_obj  # Добавляем информацию о пагинации для шаблона
+        context['paginator'] = paginator  # Пагинатор для использования в шаблоне
+        return context
