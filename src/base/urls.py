ebb8d2376cc53aac422456b31c8f5d8aa779092a
shiro-kreishi <vadas25@yandex.ru>
2024-11-23 22:40:25 +0500

    fix media

--- src/base/urls.py
+++ src/base/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from apps.users.views import home
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='homepage'),
     path('users/', include('apps.users.urls')),
     path('api/v0/', include('apps.api_v0.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
